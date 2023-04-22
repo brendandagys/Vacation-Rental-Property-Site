19aa70c1fe6cd9321f010870a2ff6239c90dbfc9
Brendan Dagys <brendandagys@gmail.com>
2023-04-22 11:00:14 -0400

    Try to get admin page working...

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,4 +1,15 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import { CalendarsContainer } from '../components/calendar/CalendarsContainer';
 
 export const AdminPage = () => (
-  <>AdminPage</>
+  <Container fluid='md'>
+    <Row>
+      <Col xs={12}>
+        <CalendarsContainer onDateRangeSelected={(from, to) => console.log({ from, to })} />
+      </Col>
+    </Row>
+  </Container>
 );
