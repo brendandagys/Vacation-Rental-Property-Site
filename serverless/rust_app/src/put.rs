c10abe84e457ebcf7ae4dbc434c43c4c02e84cf3
Brendan Dagys <brendandagys@gmail.com>
2023-04-24 19:35:24 -0400

    Allow PUT requests for booking inquiries

--- serverless/rust_app/src/put.rs
+++ serverless/rust_app/src/put.rs
@@ -26,57 +26,69 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn put(request: Request, client: &dynamodb::Client) -> Result<Response<Body>, Error> {
-    let token = match request.headers().get("Authorization") {
-        Some(token) => token,
-        None => {
-            return utils::http::send_error(
-                StatusCode::UNAUTHORIZED,
-                "No `Authorization` header provided.",
-            )
-        }
-    };
-
-    match utils::auth::validate_token::<types::auth::JwtClaims>(token.to_str().unwrap()) {
-        Ok(_) => {}
-        Err(error) => {
-            println!("Error validating token: {error}");
-            return utils::http::send_error(StatusCode::UNAUTHORIZED, "Invalid token.");
-        }
-    };
-
     match request.body() {
         Body::Text(body) => match serde_json::from_str::<types::PutRequestEntity>(body) {
-            Ok(put_request_entity) => match put_request_entity {
-                types::PutRequestEntity::BookingInquiryRequest(booking_inquiry) => {
-                    put_handlers::booking_inquiry::put_booking_inquiry(client, booking_inquiry)
-                        .await
-                }
-                types::PutRequestEntity::CalendarDateRequest(calendar_date) => {
-                    put_handlers::calendar_date::put_calendar_date(client, calendar_date).await
-                }
-                types::PutRequestEntity::ContentRequest(content) => {
-                    put_handlers::content::put_content(client, content).await
-                }
-                types::PutRequestEntity::DefaultRequest(default) => {
-                    put_handlers::default::put_default(client, default).await
-                }
-                types::PutRequestEntity::TestimonialRequest(testimonial) => {
-                    put_handlers::testimonial::put_testimonial(client, testimonial).await
-                }
-                types::PutRequestEntity::UserRequest(user) => {
-                    put_handlers::user::put_user(client, user).await
+            Ok(put_request_entity) => {
+                if let types::PutRequestEntity::BookingInquiryRequest(booking_inquiry) =
+                    put_request_entity
+                {
+                    return put_handlers::booking_inquiry::put_booking_inquiry(
+                        client,
+                        booking_inquiry,
+                    )
+                    .await;
                 }
 
-                types::PutRequestEntity::CalendarDatesRequest(calendar_dates) => {
-                    put_handlers::calendar_date::put_calendar_dates(client, calendar_dates).await
+                let token = match request.headers().get("Authorization") {
+                    Some(token) => token,
+                    None => {
+                        return utils::http::send_error(
+                            StatusCode::UNAUTHORIZED,
+                            "No `Authorization` header provided.",
+                        )
+                    }
+                };
+
+                match utils::auth::validate_token::<types::auth::JwtClaims>(token.to_str().unwrap())
+                {
+                    Ok(_) => {}
+                    Err(error) => {
+                        println!("Error validating token: {error}");
+                        return utils::http::send_error(StatusCode::UNAUTHORIZED, "Invalid token.");
+                    }
+                };
+
+                match put_request_entity {
+                    types::PutRequestEntity::CalendarDateRequest(calendar_date) => {
+                        put_handlers::calendar_date::put_calendar_date(client, calendar_date).await
+                    }
+                    types::PutRequestEntity::ContentRequest(content) => {
+                        put_handlers::content::put_content(client, content).await
+                    }
+                    types::PutRequestEntity::DefaultRequest(default) => {
+                        put_handlers::default::put_default(client, default).await
+                    }
+                    types::PutRequestEntity::TestimonialRequest(testimonial) => {
+                        put_handlers::testimonial::put_testimonial(client, testimonial).await
+                    }
+                    types::PutRequestEntity::UserRequest(user) => {
+                        put_handlers::user::put_user(client, user).await
+                    }
+
+                    types::PutRequestEntity::CalendarDatesRequest(calendar_dates) => {
+                        put_handlers::calendar_date::put_calendar_dates(client, calendar_dates)
+                            .await
+                    }
+                    types::PutRequestEntity::ContentsRequest(contents) => {
+                        put_handlers::content::put_contents(client, contents).await
+                    }
+                    types::PutRequestEntity::DefaultsRequest(defaults) => {
+                        put_handlers::default::put_defaults(client, defaults).await
+                    }
+
+                    _ => panic!("Unreachable"),
                 }
-                types::PutRequestEntity::ContentsRequest(contents) => {
-                    put_handlers::content::put_contents(client, contents).await
-                }
-                types::PutRequestEntity::DefaultsRequest(defaults) => {
-                    put_handlers::default::put_defaults(client, defaults).await
-                }
-            },
+            }
             Err(error) => utils::http::send_error(
                 StatusCode::BAD_REQUEST,
                 &format!("Please provide a valid entity. ERROR: {error}"),
