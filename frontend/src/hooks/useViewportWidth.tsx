f61bddd7f15e8e4802925ac9097efe98d99881b1
Brendan Dagys <brendandagys@gmail.com>
2023-05-06 21:47:24 -0400

    Add `useViewportWidth()` hook

---
+++ frontend/src/hooks/useViewportWidth.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export const useViewportWidth = (): number => {
+  const [ width, setWidth ] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return width;
+};
