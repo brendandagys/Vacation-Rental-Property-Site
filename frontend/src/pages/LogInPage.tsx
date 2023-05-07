d44ad8ad780080ee3be1f50b55c5243f7a0e9cc8
Brendan Dagys <brendandagys@gmail.com>
2023-05-06 22:02:06 -0400

    Improve Log In page 'Home' button placement

--- frontend/src/pages/LogInPage.tsx
+++ frontend/src/pages/LogInPage.tsx
@@ -8,11 +8,8 @@ export const LogInPage = () => {
   return (
     <Container className="p-4 mt-3">
       <Row className="justify-content-center">
-        <Col>
-          <Button
-            variant='warning'
-            onClick={() => { setPath('/'); }}
-          >
+        <Col xs={10} sm={8} md={6} lg={4} xl={3}>
+          <Button variant='warning' size='lg' onClick={() => { setPath('/'); }}>
             Home
           </Button>
         </Col>
@@ -20,9 +17,7 @@ export const LogInPage = () => {
 
       <Row className='justify-content-center'>
         <Col xs={10} sm={8} md={6} lg={4} xl={3}>
-          <div className="bg-white rounded p-4 mt-5">
-            <LogInForm />
-          </div>
+          <div className="bg-white rounded p-4 mt-5"><LogInForm /></div>
         </Col>
       </Row>
     </Container>
