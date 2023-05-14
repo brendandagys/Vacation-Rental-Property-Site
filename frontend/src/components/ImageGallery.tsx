e3d8c5407c1b3e97620c984676c833de6a7ce390
Brendan Dagys <brendandagys@gmail.com>
2023-05-14 09:59:49 -0400

    Improve Image Galley button size

--- frontend/src/components/ImageGallery.tsx
+++ frontend/src/components/ImageGallery.tsx
@@ -4,6 +4,7 @@ import { faCircleChevronLeft, faCircleChevronRight, faCircleXmark } from '@forta
 import { Nullable } from '../types';
 import { Col } from 'react-bootstrap';
 import { FadeInImage } from './FadeInImage';
+import { useViewportWidth } from '../hooks/useViewportWidth';
 
 interface ImageGalleryProps {
   images: string[];
@@ -13,6 +14,8 @@ export const ImageGallery = ({ images }: ImageGalleryProps) => {
   const [ slideNumber, setSlideNumber ] = useState<Nullable<number>>(null);
   const [ showModal, setShowModal ] = useState(false);
 
+  const width = useViewportWidth();
+
   return (
     <div>
       <Col xs={12} className='text-center text-white mb-5'><h1 className='font-5xl'>Gallery</h1></Col>
@@ -20,25 +23,40 @@ export const ImageGallery = ({ images }: ImageGalleryProps) => {
       {
         showModal && (
           <div className="slider-wrap" onClick={() => { setShowModal(false); }}>
-            <FontAwesomeIcon icon={faCircleXmark} className='btn-close' onClick={() => setShowModal(false)} />
-            <FontAwesomeIcon icon={faCircleChevronLeft} className='btn-previous' onClick={(e) => {
-              e.stopPropagation();
+            <FontAwesomeIcon
+              size={width < 400 ? '2x' : '3x'}
+              icon={faCircleXmark}
+              className='btn-close'
+              onClick={() => setShowModal(false)}
+            />
+            <FontAwesomeIcon
+              size={width < 400 ? '2x' : '3x'}
+              icon={faCircleChevronLeft}
+              className='btn-previous'
+              onClick={(e) => {
+                e.stopPropagation();
 
-              setSlideNumber((old) => (
-                old !== null
-                  ? old === 0 ? images.length - 1 : old - 1
-                  : old
-              ));
-            }} />
-            <FontAwesomeIcon icon={faCircleChevronRight} className='btn-next' onClick={(e) => {
-              e.stopPropagation();
+                setSlideNumber((old) => (
+                  old !== null
+                    ? old === 0 ? images.length - 1 : old - 1
+                    : old
+                ));
+              }}
+            />
+            <FontAwesomeIcon
+              size={width < 400 ? '2x' : '3x'}
+              icon={faCircleChevronRight}
+              className='btn-next'
+              onClick={(e) => {
+                e.stopPropagation();
 
-              setSlideNumber((old) => (
-                old !== null ?
-                  old === images.length - 1 ? 0 : old + 1
-                  : old
-              ));
-            }} />
+                setSlideNumber((old) => (
+                  old !== null ?
+                    old === images.length - 1 ? 0 : old + 1
+                    : old
+                ));
+              }}
+            />
 
             <div className="fullscreen"><img src={images[slideNumber ?? 0]} alt='' /></div>
           </div>
