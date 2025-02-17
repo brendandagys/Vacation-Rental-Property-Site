575be1b645b9d92fb1998fd2d97435883fa6b56c
Brendan Dagys <brendandagys@gmail.com>
2025-02-17 12:55:43 -0500

    Revert video tour speed back to normal

--- frontend/src/components/Video.tsx
+++ frontend/src/components/Video.tsx
@@ -1,30 +1,30 @@
-import { useRef } from 'react';
-import { tourVideo } from '../static/images';
-import { useViewportWidth } from '../hooks/useViewportWidth';
+import { useRef } from "react";
+import { tourVideo } from "../static/images";
+import { useViewportWidth } from "../hooks/useViewportWidth";
 
 export const Video = () => {
   const { width } = useViewportWidth();
 
   const videoRef = useRef(null);
 
-  const setPlayback = () => {
-    if (videoRef.current) {
-      (videoRef.current as { playbackRate: number; }).playbackRate = 1.5;
-    }
-  };
+  // const setPlayback = () => {
+  //   if (videoRef.current) {
+  //     (videoRef.current as { playbackRate: number }).playbackRate = 1.5;
+  //   }
+  // };
 
   return (
     <div id="tour" className="d-flex justify-content-center bg-black">
       <video
         controls
         autoPlay
-        width={width < 1200 ? '100%' : '70%'}
+        width={width < 1200 ? "100%" : "70%"}
         height="100%"
         id="video"
         loop
-        style={{ objectFit: 'cover' }}
+        style={{ objectFit: "cover" }}
         muted
-        onCanPlay={setPlayback}
+        // onCanPlay={setPlayback}
         ref={videoRef}
       >
         <source src={tourVideo} type="video/mp4" />
